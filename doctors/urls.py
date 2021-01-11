ec73de514f16f01b5e81e0575433938b03b596ea
KacperMitkowski <kacper.mitkowski@gmail.com>
2021-01-11 22:05:20 +0100

    reafctor + screenshots added

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('doctor/<int:page_nr>/', views.doctor_list_page, name='doctor_list_page'),
     path('doctor/details/<str:doctor_id>/', views.doctor_details, name='doctor_details'),
-    path('api/load_doctors_to_db_from_csv', views.load_doctors_to_db_from_csv, name='load_doctors'),
-    path('api/load_doctor_addresses_from_csv', views.load_doctor_addresses_from_csv, name='load_addresses')
+    path('load_doctors_to_db_from_csv', views.load_doctors_to_db_from_csv, name='load_doctors'),
+    path('load_doctor_addresses_from_csv', views.load_doctor_addresses_from_csv, name='load_addresses')
 ]
